ba061b20e05b06a798f299ed2ae57eb3b97d56e0
danbamitale <danbamitale@gmail.com>
2025-08-28 00:35:18 +0100

    fix bin array field logging

--- src/main/java/com/titp/server/utils/IsoLogger.java
+++ src/main/java/com/titp/server/utils/IsoLogger.java
@@ -1,8 +1,11 @@
 package com.titp.server.utils;
 
+import ch.qos.logback.core.encoder.ByteArrayUtil;
 import com.solab.iso8583.IsoMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
 
 public class IsoLogger {
     private static final Logger logger = LoggerFactory.getLogger(IsoLogger.class);
@@ -20,8 +23,16 @@ public class IsoLogger {
             boolean hasFields = false;
             for (int i = 2; i <= 128; i++) {
                 if (message.hasField(i)) {
-                    String fieldValue = String.valueOf(message.getField(i).getValue());
-                    if (fieldValue != null) {
+                    var field = message.getField(i);
+                    var value = field.getValue();
+                    if (value != null) {
+                        String fieldValue;
+                        if (value.getClass().isArray()) {
+                            fieldValue = ByteArrayUtil.toHexString((byte[]) value).toUpperCase();
+                        } else {
+                            fieldValue = String.valueOf(message.getField(i).getValue());
+                        }
+
                         fieldsInfo.append(String.format("\nF%d=%s", i, fieldValue));
                         hasFields = true;
                     }
